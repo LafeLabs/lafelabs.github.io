ff629bb304e40d29d24b0092e596cd9a5e366ac9
LafeLabs <LafeLabs@gmail.com>
2017-01-14 19:59:39 -0500

    holy crap this is starting to work!!!!!!!1

--- geometron.js
+++ geometron.js
@@ -1,33 +1,304 @@
-var x = 100;
-var y = 100;
+var ASCIImode = false;
+var backgroundOn = false;
+var glyphSpellingOn = true;
+var x,y,x0,y0;
+var spellX,spellY;
+var spellSide;
+var side;
+var scaleFactor, unit;
+var theta,theta0,thetaStep;
+var currentGlyphIndex = 0;
+var currentTableIndex = 0;
+var currentGlyphAddress = 0;
+var triangleX,triangleY,squareX,squareY,pentagonX,pentagonY,hexagonX,hexagonY;
 
+var tableMode = 4;  
+//1: font
+//2: shape symbols
+//16(0020): shape actions
+//3: command symbols
+//4: manuscript actions 
+//5: manuscript symbols
+
+var phi = 0.5*(sqrt(5) + 1);
+
+var currentGlyphString = "";
+
+
+//"~" is 0176 in octal and switches between ASCIImode true and false, should not go here
+var keyRow0 = ['1','2','3','7','0','-','='];
+var keyAddressRow0 = [0304,0305,0306,0317,0300,0336,0337];
+var keyRow1 = ['q','w','e','r','t','u','i','o','p'];
+var keyAddressRow1 = [0310,0311,0312,0313,0314,0370,0371,0360,0361];
+var keyRow2 = ['a','s','d','f','g','h','j','k','l',';'];
+var keyAddressRow2 = [0330,0331,0332,0333,0334,0335,0350,0351,0352,0353];
+var keyRow3 = ['z','x','c','v'];
+var keyAddressRow3 = [0340,0341,0342,0343];
+var keyRow4 = ['!','@','#','$','%','^','&','*','Q','W','E','R','T','Y','U','I'];
+var keyAddressRow4 = [0200,0201,0202,0203,0204,0205,0206,0207,0210,0211,0212,0213,0214,0215,0216,0217];
 
 
 function setup() {
+
+  ellipseMode(CENTER);
+  noFill();
+  strokeWeight(1);
+  unit = 75;
+  scaleFactor = 2;
+  side = 50;
+  thetaStep = PI/2;
+  theta0 = -PI/2; 
+  theta = theta0;
   createCanvas(640, 480);
+  x0 = 250;
+  y0 = 250;
+  x = x0;
+  y = y0;
+  
+  spellX = 10;
+  spellY = height - 100;
+  spellSide = 20;
+
+  triangleX = x0;
+  triangleY = y0;
+  squareX = x0;
+  squareY = y0;
+  pentagonX = x0;
+  pentagonY = y0;
+  hexagonX = x0;
+  hexagonY = y0;
+  noFill();
+
 
 }
 
 function draw() {
-background(255);
-noFill();
-
-  ellipse(x, y, 50, 50);
+	background(255);
+    drawCursor();
 }
 
 function keyTyped(){
 
-	if(key === 'f'){
-		x += 10;
-	}
-	if(key === 'd'){
-		x -= 10;
-	}
 	if(key === 'a'){
-		y += 10;
+		doTheThing(0330);
 	}
 	if(key === 's'){
-		y -= 10;
+		doTheThing(0331);
 	}
+	if(key === 'd'){
+		doTheThing(0332);
+	}
+	if(key === 'f'){
+		doTheThing(0333);
+	}
+	if(key === 'g'){
+		doTheThing(0334);
+	}
+	if(key === 'h'){
+		doTheThing(0335);
+	}
+	if(key === 'j'){
+		doTheThing(0336);
+	}
+	if(key === 'k'){
+		doTheThing(0337);
+	}
+
+}
+
+function drawCursor(){
+  stroke(color(255,0,0));
+  line(x,y,x + scaleFactor*side*cos(theta),y+scaleFactor*side*sin(theta));
+  strokeWeight(3);
+  stroke(0);
+  line(x,y,x + side*cos(theta),y + side*sin(theta));
+  stroke(color(0,0,255));
+  strokeWeight(4);
+  line(x,y,x + (side/scaleFactor)*cos(theta),y+(side/scaleFactor)*sin(theta));
+  strokeWeight(1);
+  stroke(0);
+  line(x,y,x + side*cos(theta + thetaStep),y+side*sin(theta + thetaStep));
+  line(x,y,x + side*cos(theta - thetaStep),y+side*sin(theta - thetaStep));
+}
+
+
+function key2command(localChar){
+    var localInt = -1;
+    
+    for(index = 0;index < keyRow0.length; index++){
+     if(localChar == keyRow0[index]){
+         localInt = keyAddressRow0[index];
+     }
+  }
+  for(index = 0;index < keyRow1.length; index++){
+     if(localChar == keyRow1[index]){
+         localInt = keyAddressRow1[index];
+     }
+  }
+  for(index = 0;index < keyRow2.length; index++){
+     if(localChar == keyRow2[index]){
+         localInt = keyAddressRow2[index];
+     }
+  }
+  for(index = 0;index < keyRow3.length; index++){
+     if(localChar == keyRow3[index]){
+         localInt = keyAddressRow3[index];
+     }
+  }
+  for(index = 0;index < keyRow4.length; index++){
+     if(localChar == keyRow4[index]){
+         localInt = keyAddressRow4[index];
+     }
+  }
+  return localInt;
+}
+
+
+function doTheThing(localCommand){
+    
+
+
+    //geometric native action commands
+    if(localCommand == 0300){
+      x = x0;
+      y = y0;
+      theta = theta0;
+      side = unit;
+      thetaStep = PI/2;
+      scaleFactor = 2;
+    }
+    if(localCommand == 0304){
+      thetaStep = PI/2;
+    }
+    if(localCommand == 0305){
+      thetaStep = 2*PI/5;
+    }
+    if(localCommand == 0306){
+      thetaStep = PI/3;
+    }    
+    if(localCommand == 0310){
+       scaleFactor = sqrt(2); 
+    }
+    if(localCommand == 0311){
+       scaleFactor = phi; //"golden" ratio 
+    }
+    if(localCommand == 0312){
+       scaleFactor = sqrt(3); 
+    }
+    if(localCommand == 0313){
+      scaleFactor = 2;  //2x
+    }
+    if(localCommand == 0314){
+      scaleFactor = 3;  //3x
+    }
+    if(localCommand == 0317){
+       side = unit; 
+    }
+    
+    if(localCommand == 0330){
+      x += side*cos(theta);   
+      y += side*sin(theta); 
+    }
+    if(localCommand == 0331){
+      x -= side*cos(theta);   
+      y -= side*sin(theta); 
+    }
+    if(localCommand == 0332){
+      x += side*cos(theta - thetaStep);
+      y += side*sin(theta - thetaStep);
+    }
+    if(localCommand == 0333){
+      x += side*cos(theta + thetaStep);
+      y += side*sin(theta + thetaStep);
+    }
+    if(localCommand == 0334){
+      theta -= thetaStep; // CCW
+    }
+    if(localCommand == 0335){
+      theta += thetaStep; // CW
+    }
+    if(localCommand == 0336){
+      side /= scaleFactor; // -
+    }
+    if(localCommand == 0337){
+      side *= scaleFactor; // +
+    }
+    if(localCommand == 0340){
+      strokeWeight(5);
+      point(x,y);
+      strokeWeight(1);//point
+    }
+    if(localCommand == 0341){
+      ellipse(x,y,2*side,2*side);//circle
+    }
+    if(localCommand == 0342){
+      line(x,y,x + side*cos(theta),y + side*sin(theta));//line
+    }
+    if(localCommand == 0343){
+        arc(x,y,2*side,2*side,theta - thetaStep,theta + thetaStep);//arc
+    }
+    if(localCommand == 0350){
+      thetaStep /= 2;  //angle/2
+    }
+    if(localCommand == 0351){
+      thetaStep *= 2;  //2angle
+    }
+    if(localCommand == 0352){
+      thetaStep /= 3; //angle/3
+    }
+    if(localCommand == 0353){
+      thetaStep *= 3; //3angle
+    }
+    if(localCommand == 0360){//grab image
+      myImage = get(int(x),int(y),int(side),int(side));
+    }
+    if(localCommand == 0361){//drop image
+       image(myImage,x,y,int(side),int(side));
+    }
+    if(localCommand == 0370){ //drop triangle marker
+        triangleX = x;
+        triangleY =y;
+    }
+    if(localCommand == 0371){//go to triangle marker
+        x = triangleX;
+        y = triangleY;
+    }
+    if(localCommand == 0372){//drop square marker
+        squareX = x;
+        squareY = y;
+    }
+    if(localCommand == 0373){//go to square marker
+        x = squareX;
+        y = squareY;
+    }
+    if(localCommand == 0374){//drop pentagon marker
+        pentagonX = x;
+        pentagonY = y;
+    }
+    if(localCommand == 0375){//go to pentagon marker
+        x = pentagonX;
+        y = pentagonY;
+    }
+    if(localCommand == 0376){//drop hexagon marker
+        hexagonX = x;
+        hexagonY = y;
+    }
+    if(localCommand == 0377){//go to hexagon marker
+        x = hexagonX;
+        y = hexagonY;
+    }
+    
+        if(x > width){
+      x=0;
+    }
+    if(x<0){
+       x = width; 
+    }
+    if(y>height){
+      y=0;
+    }
+    if(y<0){
+      y = height;
+    }
 
 }
