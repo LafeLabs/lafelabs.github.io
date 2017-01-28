a28e1e7cb2a6e124bf8cf1a2c1959116e5214246
LafeLabs <LafeLabs@gmail.com>
2017-01-27 19:44:54 -0500

    making manuscript make sense for schematic drawing

--- libraries/geometronJSlibrary.js
+++ libraries/geometronJSlibrary.js
@@ -64,7 +64,7 @@ function setGeometronGlobals(){
   currentGlyphTable = [];
   currentGlyphAddress = 0400;//int
   currentTableIndex = 0;
-  cursorString = "gchchcg";
+  cursorString = "gchhcgc1rjahhh--chhchhhk==s";
   manuscriptPageindex = 0;
 
   imageTableIndex = 0;
@@ -233,21 +233,21 @@ function loadShapes(){
     shapeActions.push("0205:2w=cahh-cahc");
     shapeActions.push("0206:z");
     shapeActions.push("0207:0aadddd-~0207~0");
-    shapeActions.push("0210:--as-asca3jhhcggahhhhcggggggchhhgaggggchhhhhhcggahhhhcggggggchhahgggggchhhhhhcggahhhhcggggggchhaggggchhhhca21===");//resistor
-    shapeActions.push("0211:---ca=cahcggchagcggcgca-ca===");//capacitor
-    shapeActions.push("0212:r--hcggcajhhhq=cahhcahhhk-agr==");//ground
-    shapeActions.push("0213:t--nf-f=b-ddggbnhhff===");//loop in a transformer
-    shapeActions.push("0214:----ca====t-=g--fb==RRRR--nfhca----ca======r");//inductor
-    shapeActions.push("0215:ct-ar-d1jh=q=c-fgg=c-r-dhkaaaat=r=");//"x" josephson junction
-    shapeActions.push("0216:t-cagr-cggchh=thr-d1j=c-h=q=c-fgcg=c-r-dhkaagchhcgcacat=r=");//xbox jj
+    shapeActions.push("0210:1--as-asca3jhhcggahhhhcggggggchhhgaggggchhhhhhcggahhhhcggggggchhahgggggchhhhhhcggahhhhcggggggchhaggggchhhhca21===");//resistor
+    shapeActions.push("0211:1---ca=cahcggchagcggcgca-ca===");//capacitor
+    shapeActions.push("0212:1r--hcggcajhhhq=cahhcahhhk-agr==");//ground
+    shapeActions.push("0213:1t--nf-f=b-ddggbnhhff===");//loop in a transformer
+    shapeActions.push("0214:1----ca====t-=g--fb==RRRR--nfhca----ca======r");//inductor
+    shapeActions.push("0215:1ct-ar-d1jh=q=c-fgg=c-r-dhkaaaat=r=");//"x" josephson junction
+    shapeActions.push("0216:1t-cagr-cggchh=thr-d1j=c-h=q=c-fgcg=c-r-dhkaagchhcgcacat=r=");//xbox jj
     shapeActions.push("0217:r1--ax---sssdddds===~V~sd==---fa===");
 
-    shapeActions.push("0220:r--gchhcgt-aahcggcdcacssscacahs=r==gg");
-    shapeActions.push("0221:--m,ggm,hca---axs===s-scahaaaah===cfca");
-    shapeActions.push("0222:t-r-caaaaacss=xd-jhchhchhchhc=hkff-jgchhchhchhchhhkddaaa=t=r");
-    shapeActions.push("0223:--caaacssg=chhc-gaahjh=q=chhchhhk-r-a==");
-    shapeActions.push("0224:r---ca=axaxa-ca===");
-    shapeActions.push("0225:r---ca=cagchhcgahchhcjgq=cggcgggk-rca-ca===");
+    shapeActions.push("0220:1r--gchhcgt-aahcggcdcacssscacahs=r==gg");
+    shapeActions.push("0221:1--m,ggm,hca---axs===s-scahaaaah===cfca");
+    shapeActions.push("0222:1t-r-caaaaacss=xd-jhchhchhchhc=hkff-jgchhchhchhchhhkddaaa=t=r");
+    shapeActions.push("0223:1--caaacssg=chhc-gaahjh=q=chhchhhk-r-a==");
+    shapeActions.push("0224:1r---ca=axaxa-ca===");
+    shapeActions.push("0225:1r---ca=cagchhcgahchhcjgq=cggcgggk-rca-ca===");
     shapeActions.push("0226:0");
     shapeActions.push("0227:0");
 
